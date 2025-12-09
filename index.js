2380f48457b9c3e886e109bc827813b3dcf14307
swilli69-jpg <swilli69@pratt.edu>
2025-12-08 20:01:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function setupBookHovers() {
       else if (alt.includes("Goodnight Moon")) bookId = 'goodnightmoon';
       else if (alt.includes("Lawn Boy")) bookId = 'lawnboy';
       else if (alt.includes("All Boys")) bookId = 'allboys';
-      else if (alt.includes("Wallflower")) bookId = 'wallflower';
+      else if (alt.includes("Price of Salt")) bookId = 'priceofsalt';
       else if (alt.includes("This Book Is Gay")) bookId = 'thisbookisgay';
       else if (alt.includes("Flamer")) bookId = 'flamer';
       else if (alt.includes("Dying Girl")) bookId = 'dyinggirl';
